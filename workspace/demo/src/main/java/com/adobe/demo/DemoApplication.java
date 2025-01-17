ef91f5033f801c2a444b7851cdbcd40345626b41
Banuprakash <banuprakashc@yahoo.co.in>
2025-01-17 16:34:32 +0530

    day 1

--- workspace/demo/src/main/java/com/adobe/demo/DemoApplication.java
+++ workspace/demo/src/main/java/com/adobe/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.adobe.demo;
 
+import com.adobe.demo.service.AppService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+        String[] names = ctx.getBeanDefinitionNames(); // all beans in Spring Container
+        for(String name: names) {
+            System.out.println(name);
+        }
+
+        System.out.println("*****");
+        AppService service = ctx.getBean("appService", AppService.class);
+        service.insert();
     }
 
 }
---
+++ workspace/demo/src/main/java/com/adobe/demo/service/AppService.java
@@ -0,0 +1,16 @@
+package com.adobe.demo.service;
+
+import com.adobe.demo.dao.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+    @Autowired
+    private BookRepo bookRepo; // not tightly coupled
+    // any implementation of BookRepo can be injected, DI by type
+
+    public void insert() {
+        this.bookRepo.addBook();
+    }
+}
